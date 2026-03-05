2656c6a4ec05d9a4599cbb277e69577daed2272e
Michał Boek <kgm9197@gmail.com>
2026-03-05 23:31:31 +0100

    working almost

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -8,21 +8,13 @@
  * @module
  */
 
-import type * as http from "../http.js";
-import type * as posts from "../posts.js";
-import type * as users from "../users.js";
-
 import type {
   ApiFromModules,
   FilterApi,
   FunctionReference,
 } from "convex/server";
 
-declare const fullApi: ApiFromModules<{
-  http: typeof http;
-  posts: typeof posts;
-  users: typeof users;
-}>;
+declare const fullApi: ApiFromModules<{}>;
 
 /**
  * A utility for referencing Convex functions in your app's public API.
